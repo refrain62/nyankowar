97785d8454e80e8617d1e9c02bcf8a24718ad5cf
refrain62 <nao_h13@hotmail.com>
2026-03-08 20:33:03 +0900

    GitHub ActionsでPagesにデプロイできるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // GitHub Pages用に相対パスでビルドするように設定
 })
